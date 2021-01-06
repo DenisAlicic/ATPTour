4df8bea6e58fc4fe5aee99f2ae8748f5f58e3deb
mrdakj <mrdakj@gmail.com>
2021-01-06 19:28:28 +0100

    adding matches and players controller

---
+++ TennisAssociation/Controllers/MatchesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisAssociation.Models;
+
+namespace TennisAssociation.Controllers
+{
+    [Route("api/matches")]
+    [ApiController]
+    public class MatchesController : Controller
+    {
+        private readonly TennisAssociationContext db;
+        public MatchesController(TennisAssociationContext context)
+        {
+            db = context;
+        }
+
+        // GET api/matches/
+        /// <summary>
+        /// Return all matches from database.
+        /// </summary>
+        /// <returns>json containing information about matches</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var matches = db.Matches.ToList();
+            return Json(matches);
+        }
+
+        // GET api/matches/<id>
+        /// <summary>
+        /// Return a match based on id
+        /// </summary>
+        /// <param name="id">id of a match</param>
+        /// <returns>json of match</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var match = db.Matches.Where(b => b.Id == id).First();
+            return Json(match);
+        }
+    }
+}
---
+++ TennisAssociation/Controllers/PlayersController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisAssociation.Models;
+
+namespace TennisAssociation.Controllers
+{
+    [Route("api/players")]
+    [ApiController]
+    public class PlayersController : Controller
+    {
+        private readonly TennisAssociationContext db;
+        public PlayersController(TennisAssociationContext context)
+        {
+            db = context;
+        }
+
+        // GET api/players/
+        /// <summary>
+        /// Return all players from database.
+        /// </summary>
+        /// <returns>json containing information about players</returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var players = db.Players.ToList();
+            return Json(players);
+        }
+
+        // GET api/players/<firstName>/<lastName>
+        /// <summary>
+        /// Return player(s) with given name.
+        /// </summary>
+        /// <param name="firstName">Player's first name.</param>
+        /// <param name="lastName">Player's last name.</param>
+        /// <returns>json of players</returns>
+        [HttpGet("{firstName}/{lastName}")]
+        public IActionResult Get(string firstName, string lastName)
+        {
+            var players = db.Players
+                .Where(b => b.FirstName == firstName && b.LastName == lastName)
+                .ToList();
+            return Json(players);
+        }
+
+        // GET api/players/<id>
+        /// <summary>
+        /// Return a player based on id
+        /// </summary>
+        /// <param name="id">id of player</param>
+        /// <returns>json of player</returns>
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var player = db.Players.Where(b => b.Id == id).First();
+            return Json(player);
+        }
+    }
+}
